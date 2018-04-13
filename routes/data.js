6cf8aede7c7c4d2e1ff79935e6ee758208c0d18d
hanlf <hanlf@hanlfdeMacBook-Pro.local>
2018-04-13 13:02:06 +0800

    目录读取-data.js

--- routes/data.js
+++ routes/data.js
@@ -2,12 +2,14 @@ var express = require('express');
 var router = express.Router();
 var pg1 = require('./pgconn');
 var path = require('path')
+var fs = require('fs')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+// 获取初步设计文件目录视图数据
 router.get('/lf1', function(req, res, next) {
   process.env.TZ = "Asia/Shanghai";
   sql ="select * from doc_view order by \"order\"";
@@ -18,5 +20,16 @@ router.get('/lf1', function(req, res, next) {
   });
 
 });
+
+router.get('/input_file', function(req, res, next) {
+  fs.readdir("./public/files/TMSR-LF1工程/初步设计/01.设计输入参数",function(err,files){
+    console.log(files);
+  })
+  
+
+
+});
+
+
 
 module.exports = router;
