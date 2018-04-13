5faa4303a7ed0009c3420a45d1ee9a6fd2bd761c
hanlf <hanlf@hanlfdeMacBook-Pro.local>
2018-04-13 13:29:12 +0800

    获取文件类型-data.js

--- routes/data.js
+++ routes/data.js
@@ -4,6 +4,43 @@ var pg1 = require('./pgconn');
 var path = require('path')
 var fs = require('fs')
 
+//遍历文件夹，获取所有文件夹里面的文件信息
+/*
+ * @param path 路径
+ *
+ */
+
+function getFileList(path)
+{
+   var filesList = [];
+   readFile(path,filesList);
+   return filesList;
+}
+
+//遍历读取文件
+function readFile(path,filesList)
+{
+   files = fs.readdirSync(path);//需要用到同步读取
+   files.forEach(walk);
+   function walk(file)
+   {  
+        states = fs.statSync(path+'/'+file);         
+        if(states.isDirectory())
+        {
+            readFile(path+'/'+file,filesList);
+        }
+        else
+        {   
+            //创建一个对象保存信息
+            var obj = new Object();
+            obj.size = states.size;//文件大小，以字节为单位
+            obj.name = file;//文件名
+            obj.path = path+'/'+file; //文件绝对路径
+            filesList.push(obj);
+        }     
+    }
+}
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -22,9 +59,10 @@ router.get('/lf1', function(req, res, next) {
 });
 
 router.get('/input_file', function(req, res, next) {
-  fs.readdir("./public/files/TMSR-LF1工程/初步设计/01.设计输入参数",function(err,files){
+  // fs.readdir("./public/files/TMSR-LF1工程/初步设计/01.设计输入参数",function(err,files){
     // console.log(files);
-    res.jsonp(files);
+    filelist=getFileList("./public/files/TMSR-LF1工程/初步设计/01.设计输入参数");
+    res.jsonp(filelist);
   })
   
 
