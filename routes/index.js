b5e99925c5bb81ad4b719645765b64f34a4a34bf
Johnson-Han <hanlifeng@sinap.ac.cn>
2018-04-19 16:12:27 +0800

    表单验证

--- routes/index.js
+++ routes/index.js
@@ -18,9 +18,9 @@ router.get('/add_new_contact', function (req, res, next) {
 
 router.post('/lf_note_add',function(req,res,next){
   console.log(req.body);
-  // var upload = multer({ dest: '/public/files/test/' });
-  // upload.single('good')
-  // res.send({ ret_code: '0' });
+   var upload = multer({ dest: '/public/files/test/' });
+   upload.single('good')
+   res.send({ ret_code: '0' });
 })
 
 module.exports = router;
