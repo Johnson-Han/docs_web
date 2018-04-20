d818dce4693b18a27c7972dfd285c39f2b9ab07e
Johnson-Han <hanlifeng@sinap.ac.cn>
2018-04-20 15:01:40 +0800

    应该没问题了

--- routes/index.js
+++ routes/index.js
@@ -25,9 +25,9 @@ router.get('/add_new_subject/:contact_name', function (req, res, next) {
 })
 
 //按钮回复联系单的路由
-router.get('/add_reply/:reply_name', function (req, res, next) {
+router.get('/add_new_reply/:reply_name', function (req, res, next) {
   console.log(req.params)
-  // res.render('lfnewreply', { titlhe: req.params.reply_name });
+  res.render('lfnewreply', { title: req.params.reply_name });
 })
 
 
